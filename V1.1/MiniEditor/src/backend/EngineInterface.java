774e7430a08d948f80a095c31d873ebb29ccf910
Ultimate-Storm <42407324+Ultimate-Storm@users.noreply.github.com>
2020-11-09 21:32:00 +0100

    initial deployment of Selection

--- V1.1/MiniEditor/src/backend/EngineInterface.java
+++ V1.1/MiniEditor/src/backend/EngineInterface.java
@@ -3,6 +3,9 @@ package backend;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
 import backend.client.Client;
 
 /**
@@ -16,37 +19,38 @@ public class EngineInterface {
 		String userCommand;
 		Client client = new Client();                                      // Client of Command design Pattern
 		do {
-			System.out.println("Enter command.[I(Insert)/S(Select)/C(Copy)/P(Paste)/X(Cut)/D(Delete)/Q(Quit)]");                             // Available commands.
+			System.out.println("Enter command:[Insert/Select/Copy/Paste/Cut/Delete/Quit](case not sensitive)");                             // Available commands.
 			try {
 				userCommand = cmd.parseInput();
+				List<String> cmdSeg = cmd.parseInput(userCommand);
 				// Switch on the bases of user command.
-				switch (userCommand.substring(0, 1).toUpperCase()) {
-					case "I" -> {
-						client.insert(userCommand.substring(2));                               // Call insert method of client.
+				switch (cmdSeg.get(0).toUpperCase()) {
+					case "INSERT" -> {
+						client.insert(cmdSeg.get(1));                               // Call insert method of client.
 						cmd.messageDisplay(client.getBufferText(), client.getSelectionText(), client.getBufferBeginIndex(), client.getBufferEndIndex());
 					}
-					case "S" -> {
-						String[] userCommandArgs = userCommand.substring(2).split(",");
+					case "SELECT" -> {
+						String[] userCommandArgs = cmdSeg.get(1).split(",");
 						client.select(Integer.parseInt(userCommandArgs[0]), Integer.parseInt(userCommandArgs[1]));     // Call select method of client.
 						cmd.messageDisplay(client.getBufferText(), client.getSelectionText(), client.getBufferBeginIndex(), client.getBufferEndIndex());
 					}
-					case "C" -> {
+					case "COPY" -> {
 						client.copy();                                                         // Call cop method of client.
 						cmd.messageDisplay(client.getBufferText(), client.getSelectionText(), client.getBufferBeginIndex(), client.getBufferEndIndex());
 					}
-					case "P" -> {
+					case "PASTE" -> {
 						client.paste();                                                        // Call paste method of client.
 						cmd.messageDisplay(client.getBufferText(), client.getSelectionText(), client.getBufferBeginIndex(), client.getBufferEndIndex());
 					}
-					case "X" -> {
+					case "CUT" -> {
 						client.cut();                                                          // Call cut method of client.
 						cmd.messageDisplay(client.getBufferText(), client.getSelectionText(), client.getBufferBeginIndex(), client.getBufferEndIndex());
 					}
-					case "D" -> {
+					case "DELETE" -> {
 						client.delete();                                                      // Call delete method of client.
 						cmd.messageDisplay(client.getBufferText(), client.getSelectionText(), client.getBufferBeginIndex(), client.getBufferEndIndex());
 					}
-					case "Q" -> System.exit(1);                                               // Quit when user enters [q/Q] command.
+					case "QUIT" -> System.exit(1);                                               // Quit when user enters [q/Q] command.
 					default -> System.out.println("===ERROR! Please select a valid option===");
 				}
 				System.out.println("----------------------------------");
@@ -68,5 +72,8 @@ public class EngineInterface {
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));        // To read input from the console terminal.
 		return keyboard.readLine();
 	}
-
+	private List<String> parseInput(String input) {
+		String[] parts = input.split(" ");
+		return Arrays.asList(parts);
+	}
 }
